0fb37f4048667882dcbe7ff048c7544238ba7345
Mwita04 <emcee779@gmail.com>
2023-04-30 21:55:57 +0300

    final

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:slide_to_act/slide_to_act.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -12,6 +13,24 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.blue.shade300,
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: SlideAction(
+            borderRadius: 12,
+            elevation: 1,
+            innerColor: Colors.white,
+            outerColor: Colors.cyanAccent[600],
+            sliderButtonIcon: const Icon(Icons.arrow_circle_right_outlined),
+            text: 'Slide to pay',
+            onSubmit: () {
+              // TODO:  Do something
+            },
+          ),
+        ),
+      ),
+    );
   }
 }
